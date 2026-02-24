392640d3800c6798706df5341d297f23428761e2
jee <jeremiassantana50@gmail.com>
2026-02-24 11:54:29 -0300

    concertando erro no codigo

--- uteis/extrato/__init__.py
+++ uteis/extrato/__init__.py
@@ -5,7 +5,7 @@ def extrato_transacao(deposito=None, saque=None):
 
     arquivo = 'Extrato.csv'
     arquivo_exist = os.path.exists(arquivo)
-    fieldname = ['Deposito' 'Saque']
+    fieldname = ['Deposito', 'Saque']
 
     with open(arquivo, mode='a', encoding='utf-8', newline='') as extratos:
         adicionar = csv.DictWriter(extratos, fieldnames=fieldname)
